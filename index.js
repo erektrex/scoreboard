d72bf94c53e2c3b49b279a810f17c5aa9f400067
erektrex <101885803+erektrex@users.noreply.github.com>
2025-08-18 01:40:19 +0200

    commit initial files

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+
+
+let homeNumber = 0
+let guestNumber = 0
+
+function homePlus1 () {
+    
+    homeNumber += 1
+    document.getElementById("home-score").textContent = homeNumber
+    
+}
+
+function homePlus2 () {
+    
+    homeNumber += 2
+    document.getElementById("home-score").textContent = homeNumber
+    
+}
+
+function homePlus3 () {
+    
+    homeNumber += 3
+    document.getElementById("home-score").textContent = homeNumber
+    
+}
+
+function guestPlus1 () {
+    
+    guestNumber += 1
+    document.getElementById("guest-score").textContent = guestNumber
+    
+}
+
+function guestPlus2 () {
+    
+    guestNumber += 2
+    document.getElementById("guest-score").textContent = guestNumber
+    
+}
+
+function guestPlus3 () {
+    
+    guestNumber += 3
+    document.getElementById("guest-score").textContent = guestNumber
+    
+}
